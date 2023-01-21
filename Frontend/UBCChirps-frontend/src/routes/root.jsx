5154f69fe9c47587cd573c8abc8d21f2fe274a2e
terryChou28 <terrychou_28@hotmail.com>
2023-01-21 14:47:18 -0800

    first commit

--- Frontend/UBCChirps-frontend/src/routes/root.jsx
+++ Frontend/UBCChirps-frontend/src/routes/root.jsx
@@ -1,7 +1,16 @@
 export default function Root() {
   return (
     <div>
+<<<<<<< Updated upstream
       <p>this is the root!!! (main page with info?)</p>
+=======
+      <p>this is the root!!!</p>
+      <div className="feed">
+        <div className="feed__header">
+          <h2>"Home"</h2>
+        </div>
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 }
