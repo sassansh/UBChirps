672d7bb763740aa3cebc547fa7b335d94a43b001
TonyLiu0226 <TL0226yn@gmail.com>
2023-01-21 14:02:43 -0800

    adds database

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,31 @@
-const express = require('express')
+const express = require('express');
+const { MongoClient } = require("mongodb");
+
 const app = express();
 
+const url = "mongodb+srv://UBChirp:123@ubchirp.xfzetb2.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+
 app.listen(process.env.PORT || 8000)
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+    } catch (err) {
+        console.log("failed");
+    }
+    finally {
+        await client.close();
+    }
+}
 
 // ROUTES:
 app.all('/', (req, res) => {
     res.send('UBC Chirp Chirp')
+    run().catch(console.dir);
+})
+
+app.all('/posts', (req, res) => {
+    res.send('POSTS')
 })
