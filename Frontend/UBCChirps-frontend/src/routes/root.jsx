e9513827d541b9b687696c057f743da233982939
legofanclub <67127770+legofanclub@users.noreply.github.com>
2023-01-21 15:32:13 -0800

    fleshed out UserPage component

--- Frontend/UBCChirps-frontend/src/routes/root.jsx
+++ Frontend/UBCChirps-frontend/src/routes/root.jsx
@@ -1,4 +1,8 @@
 export default function Root() {
+  /**
+   * This is required, will have info about our app.
+   */
+  
   return (
     <div>
       <p>this is the root!!! (main page with info?)</p>
