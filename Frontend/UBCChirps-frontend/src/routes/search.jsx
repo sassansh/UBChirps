c73833185c40b1e4f46bfb583f034113ef8ba203
limseung1 <seungwonlim.sl@gmail.com>
2023-01-21 20:52:17 -0800

    finished search

--- Frontend/UBCChirps-frontend/src/routes/search.jsx
+++ Frontend/UBCChirps-frontend/src/routes/search.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import { useRef } from 'react'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import Navbar from '../components/navbar'
 
 const Search = () => {
+  const [users, setUsers] = useState([])
+
   return (
     <div className="search-page">
         <Navbar />
-        <SearchBar />
+        <SearchBar setUsers={setUsers}/>
+        <SearchResult users={users} />
     </div>
   )
 }
 
-const SearchBar = () => {
+const SearchBar = ({setUsers}) => {
     const inputRef = useRef();
-    const [users, setUsers] = useState([])
 
     const handleSubmit = async(e) => {
         e.preventDefault();
-        // console.log(inputRef.current.value)
         const searchValue = inputRef.current.value
 
         //fetch data
@@ -26,14 +28,13 @@ const SearchBar = () => {
         const url = `http://localhost:8000/api/users/search?name=${searchValue}`
         const res = await fetch(url);
         const users = await res.json();
-        console.log(users)
         setUsers(users);
     }
 
     return (
-      <div>
+      <div style={{padding:20}}>
         <h4>Search User</h4>
-        <form className="search-form" onSubmit={(e) => handleSubmit(e)} action="submit">
+        <form style={{display:"flex"}} onSubmit={(e) => handleSubmit(e)} action="submit">
             <input ref={inputRef} type="text" />
             <button onSubmit={(e) => handleSubmit(e)}>Submit</button>
         </form>
@@ -41,6 +42,21 @@ const SearchBar = () => {
     )
   }
 
+  const SearchResult = ({users}) => {
+    return (
+        <div style={{paddingLeft:20}}>
+        {users.map(user => 
+
+            <Link to={`../user/${user.googleId}`}>
+                <div style={{display:"flex"}}>
+                    <img style={{height: 25, width:25}} src={user.picture}/>
+                    <div>{user.displayName}</div>    
+                </div>
+            </Link>
+        )}
+        </div>
+    )
+  }
   
 
 export default Search
